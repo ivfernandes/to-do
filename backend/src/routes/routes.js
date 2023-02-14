dfffefcf9d25f4b4e9c7b7ab1b8f423e4cfe0fe7
Igor Vinicius <ivfernandes05@gmail.com>
2023-02-14 14:02:34 -0400

    Adicionando sequelize, criando migração e operações de CRUD

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import todoController from '../controllers/todo';
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/hello', (_req, res) => {
   res.json({message: 'Hello World!'});
 });
+
+// ToDo Routes
+router.get('/', todoController.index);
+router.post('/', todoController.create);
+router.get('/:id', todoController.read);
+router.put('/:id', todoController.update);
+router.delete('/:id', todoController.remove);
 
 export default router;
